809e499c6f29b620d3878bd894c5137f93aad4fe
huecester <jericho.paulino@yahoo.com.ph>
2022-06-25 20:08:53 -0700

    1.2.4

--- src/main.cpp
+++ src/main.cpp
@@ -118,41 +118,35 @@ int main() {
 
 	unsigned int shaderProgram = initializeShaderProgram();
 
-	const float VERTICES[] = {
-		0.5f, 0.5f, 0.0f,		// top right
-		0.5f, -0.5f, 0.0f,		// bottom right
-		-0.5f, -0.5f, 0.0f,		// bottom left
-		-0.5f, 0.5f, 0.0f,		// top left
+	const float VERTICES_1[] = {
+		-0.4f, -0.5f, 0.0f,
+		-0.2f, -0.5f, 0.0f,
+		-0.3f, 0.5f, 0.0f,	
 	};
-	const unsigned int INDICES[] = {
-		0, 1, 3,
-		1, 2, 3,
+	const float VERTICES_2[] = {
+		0.4f, -0.5f, 0.0f,
+		0.2f, -0.5f, 0.0f,
+		0.3f, 0.5f, 0.0f,	
 	};
 
 	// Initialize, bind, and configure vertex buffer object and vertex array object
-	unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	unsigned int VBOs[2], VAOs[2];
+	glGenVertexArrays(2, VAOs);
+	glGenBuffers(2, VBOs);
 
-	// Bind VAO
-	glBindVertexArray(VAO);
-
-	// VBO
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES), VERTICES, GL_STATIC_DRAW);
-
-	// EBO
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(INDICES), INDICES, GL_STATIC_DRAW);
-
-	// Vertex attribute
+	// VAOs[0]
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES_1), VERTICES_1, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0);
 
-	// Unbind VBO as it's referenced by the attribute buffer
-	// Keep the EBO bound as it's referenced by the VAO which is currently bound
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// VAOs[1]
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES_2), VERTICES_2, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
 
 	// Input/render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -164,8 +158,10 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(VAOs[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAOs[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// Swap buffers and poll events
 		glfwSwapBuffers(window);
@@ -173,8 +169,8 @@ int main() {
 	}
 
 	// Deallocate resources after program lifetime
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(2, VAOs);
+	glDeleteBuffers(2, VBOs);
 	glDeleteProgram(shaderProgram);
 	glfwTerminate();
 }
