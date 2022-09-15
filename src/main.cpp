40cbb39332867a91a8aa4b8320612322581668ac
huecester <jericho.paulino@yahoo.com.ph>
2022-09-15 12:32:25 -0700

    Move shaders to new source file

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,6 @@
 const unsigned int INITIAL_SCREEN_WIDTH = 800;
 const unsigned int INITIAL_SCREEN_HEIGHT = 600;
 
-const char *VERTEX_SHADER_SOURCE = "#version 330 core \
-	layout (location = 0) in vec3 aPos; \
-	layout (location = 1) in vec3 aCol; \
-	out vec3 color; \
-	void main() { \
-		gl_Position = vec4(aPos, 1.0); \
-		color = aCol; \
-	}";
-const char *FRAGMENT_SHADER_SOURCE = "#version 330 core \
-	in vec3 color; \
-	out vec4 FragColor; \
-	void main() { \
-		FragColor = vec4(color, 1.0); \
-	}";
-
 // Callbacks
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -54,59 +39,6 @@ GLFWwindow *initializeGLFW() {
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	return window;
-}
-
-bool initializeShader(GLuint type, GLsizei count, const GLchar *const *source, unsigned int &id) {
-	id = glCreateShader(type);
-	glShaderSource(id, count, source, nullptr);
-	glCompileShader(id);
-
-	int success;
-	glGetShaderiv(id, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		char infoLog[512];
-		glGetShaderInfoLog(id, 512, nullptr, infoLog);
-		std::cerr << "Shader compilation failed:\n" << infoLog << std::endl;
-		return false;
-	}
-
-	return true;
-}
-
-bool initializeShaderProgram(GLuint &id) {
-	// Initialize vertex shader
-	GLuint vertexShader;
-	if (!initializeShader(GL_VERTEX_SHADER, 1, &VERTEX_SHADER_SOURCE, vertexShader)) {
-		return false;
-	}
-
-	// Initialize fragment shader
-	GLuint fragmentShader;
-	if (!initializeShader(GL_FRAGMENT_SHADER, 1, &FRAGMENT_SHADER_SOURCE, fragmentShader)) {
-		return false;
-	}
-
-	// Create shader program
-	id = glCreateProgram();
-	glAttachShader(id, vertexShader);
-	glAttachShader(id, fragmentShader);
-	glLinkProgram(id);
-
-	// Check link status of shader program
-	GLint success;
-	glGetProgramiv(id, GL_LINK_STATUS, &success);
-	if (!success) {
-		char infoLog[512];
-		glGetShaderInfoLog(id, 512, nullptr, infoLog);
-		std::cout << "Shader program linking failed:\n" << infoLog << std::endl;
-		return false;
-	}
-
-	// Use program and free shaders
-	glUseProgram(id);
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-	return true;
 }
 
 int main() {
---
+++ src/shaders.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <glad/glad.h>
+
+const char *VERTEX_SHADER_SOURCE = "#version 330 core \
+	layout (location = 0) in vec3 aPos; \
+	layout (location = 1) in vec3 aCol; \
+	out vec3 color; \
+	void main() { \
+		gl_Position = vec4(aPos, 1.0); \
+		color = aCol; \
+	}";
+const char *FRAGMENT_SHADER_SOURCE = "#version 330 core \
+	in vec3 color; \
+	out vec4 FragColor; \
+	void main() { \
+		FragColor = vec4(color, 1.0); \
+	}";
+
+bool initializeShader(GLuint type, GLsizei count, const GLchar *const *source, unsigned int &id) {
+	id = glCreateShader(type);
+	glShaderSource(id, count, source, nullptr);
+	glCompileShader(id);
+
+	int success;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		char infoLog[512];
+		glGetShaderInfoLog(id, 512, nullptr, infoLog);
+		std::cerr << "Shader compilation failed:\n" << infoLog << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool initializeShaderProgram(GLuint &id) {
+	// Initialize vertex shader
+	GLuint vertexShader;
+	if (!initializeShader(GL_VERTEX_SHADER, 1, &VERTEX_SHADER_SOURCE, vertexShader)) {
+		return false;
+	}
+
+	// Initialize fragment shader
+	GLuint fragmentShader;
+	if (!initializeShader(GL_FRAGMENT_SHADER, 1, &FRAGMENT_SHADER_SOURCE, fragmentShader)) {
+		return false;
+	}
+
+	// Create shader program
+	id = glCreateProgram();
+	glAttachShader(id, vertexShader);
+	glAttachShader(id, fragmentShader);
+	glLinkProgram(id);
+
+	// Check link status of shader program
+	GLint success;
+	glGetProgramiv(id, GL_LINK_STATUS, &success);
+	if (!success) {
+		char infoLog[512];
+		glGetShaderInfoLog(id, 512, nullptr, infoLog);
+		std::cout << "Shader program linking failed:\n" << infoLog << std::endl;
+		return false;
+	}
+
+	// Use program and free shaders
+	glUseProgram(id);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	return true;
+}
