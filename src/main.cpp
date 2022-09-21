3249878d68db24670cb78ee6dabaa659b520759b
huecester <jericho.paulino@yahoo.com.ph>
2022-09-21 11:25:04 -0700

    shaders.cpp > shader.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "shaders.hpp"
+#include "shader.hpp"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
--- src/shader.cpp
+++ src/shader.cpp
@@ -1,4 +1,4 @@
-#include "shaders.hpp"
+#include "shader.hpp"
 
 #include <glad/glad.h>
 
